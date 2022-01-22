50ab5166cfe9f34c346208f62622d5b5453f4254
Rumer Tovar <rumertovar7474@gmail.com>
2022-01-22 09:36:45 -0500

    adding reset style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import Card from './components/Card';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+html, body{
+  margin: 0;
+  padding: 0;
+}
+`;
 
 function App() {
  return (
   <>
+   <GlobalStyle />
    <Card />
   </>
  );
--- src/components/CardBackground.js
+++ src/components/CardBackground.js
@@ -1,9 +1,16 @@
+import styled from 'styled-components';
 import BackgroundImage from '../images/pattern-background-mobile.svg';
+
+const Image = styled.img`
+ position: absolute;
+ z-index: -1;
+ width: 100vw;
+`;
 
 const CardBackground = () => {
  return (
   <>
-   <img src={BackgroundImage} alt='Background-pattern' />
+   <Image className='pattern' src={BackgroundImage} alt='Background-pattern' />
   </>
  );
 };
